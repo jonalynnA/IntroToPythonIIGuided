725eff10cc2c19a4ae8d633eeda0c0eef5d89ed1
jonalynn masters <jonalynna.masters@gmail.com>
2020-04-07 10:18:53 -0700

    UPER

---
+++ UPER.py
@@ -0,0 +1,31 @@
+# Return the " centered" average of an array of ints, which we'll say is the
+# mean average of the values, except ignoring the largest and smallest values
+# in the array
+
+# centered_avereage([1, 2, 3, 4, 100]) -> 3
+# centered_average([1, 1, 5, 5, 10, 8, 7]) -> 5
+# centered_average([-10, -4, -2, -4, -2, 0]) -> -3
+
+# What about multiple smallest values?
+# Just ignore one of them
+
+# How large?
+# No limit
+
+# Speed issues?
+# No worries
+
+# All INTs?
+# Yes
+
+# Fewer than 3 elements?
+# Will always be 3 or more
+
+# Number size:
+# Arbitrarily large
+
+# Can I use other libraries?
+# No
+
+# Will the list always be sorted?
+# No
